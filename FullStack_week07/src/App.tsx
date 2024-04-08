a9dd9e662153c848e1d9bf8c657afff6999adcd8
hrisa006 <hrisi_karaivanova@abv.bg>
2024-04-09 02:01:35 +0300

    fix

--- FullStack_week07/src/App.tsx
+++ FullStack_week07/src/App.tsx
@@ -4,6 +4,7 @@ import { Todo, TodoCreateDto } from './todo-model';
 import TodoList from './TodoList';
 import TodoRepository from './todo-repository';
 import TodoInput from './TodoInput';
+import API from './todo-api-client';
 
 interface AppState {
   todos: Todo[];
@@ -17,25 +18,51 @@ class App extends React.Component<{}, AppState> {
   }
 
   async componentDidMount() {
-      const todos = await TodoRepository.findAll();
-      this.setState({todos});
+    try {
+      const todos = await API.findAll(Todo);
+      this.setState({ todos });
+
+    } catch (err) {
+      this.showError(err as Error);
+    }
   }
 
   createTodo = async (todo: TodoCreateDto) => {
-    const created = await TodoRepository.create(todo);
-    this.setState(state => ({todos: [...state.todos, created]}));
+    try {
+      const created = await API.create(Todo, todo);
+      this.setState(state => ({ todos: [...state.todos, created] }))
+      this.clearErrors();
+    } catch (err) {
+      this.showError(err as Error);
+    }
+  }
+
+  deleteTodo = async (todo: Todo) => {
+    try {
+      const deleted = await API.deleteById(Todo, todo.id);
+      this.setState(state => ({ todos: state.todos.filter(td => td.id !== deleted.id) }))
+      this.clearErrors();
+    } catch (err) {
+      this.showError(err as Error);
+    }
   }
 
   showError = (error: Error) => {
-    this.setState({errors: error.message});
+    this.setState({ errors: error.message })
   }
+
+  clearErrors = () => {
+    this.setState({ errors: '' })
+  }
+
 
   render() {
     return (
       <div className='container d-flex flex-column justify-content-between text-black bg-light'>
-        <TodoInput onCreateTodo={this.createTodo} onError={this.showError}/>
-        {this.state.errors && <div className='errors'>{this.state.errors}</div>}
-        <TodoList todos={this.state.todos}/>
+        <TodoInput onCreateTodo={this.createTodo} onError={this.showError} />
+        {this.state.errors && <div className='errors'>Error: {this.state.errors}</div>}
+        <hr />
+        <TodoList todos={this.state.todos} onDelete={this.deleteTodo}/>
       </div>
     );
   }
