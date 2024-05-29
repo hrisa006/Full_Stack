6c4bf94c2d4a226d91ae67c9aa6710566f4d6a31
Hristiana Karaivanova <115207801+hrisa006@users.noreply.github.com>
2024-05-29 12:08:52 +0300

    Delete FullStack_week07 directory

--- FullStack_week07/src/shared-types.ts
+++
@@ -1,7 +0,0 @@
-export type IdType = number;
-
-export interface Identifiable<K> {
-    id: K;
-}
-
-export type Optional<V> = V | undefined;
--- FullStack_week07/src/todo-api-client.ts
+++
@@ -1,5 +0,0 @@
-import { ApiClient } from "./api-client";
-
-const API = new ApiClient("http://localhost:9000"); // Singleton
-
-export default API;
